e1934a2cc65f150482a5fb89e878c864141da59c
sbimochan <bmochan@gmail.com>
2019-02-23 19:58:08 +0545

    Basic setups

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './normalize.css';
+import './skeleton.css';
+import React, {useState, useEffect} from 'react';
+import TimePicker from 'react-times';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+// use material theme
+import 'react-times/css/material/default.css';
+// or you can use classic theme
+import 'react-times/css/classic/default.css';
+
+function App() {
+  const [fromHour,
+    setFromHour] = useState(9);
+  const [fromMinute,
+    setFromMinute] = useState(15);
+  const [toHour,
+    setToHour] = useState(11);
+  const [toMinute,
+    setToMinute] = useState(15);
+  const [timeDiff,
+    setTimeDiff] = useState(0);
+
+  useEffect(() => {
+    let fromTime = fromHour * 60 + fromMinute;
+    let toTime = toHour * 60 + toMinute;
+    console.log({fromTime});
+    console.log({toTime})
+    let currentDate = new Date();
+    let currentTimeZoneOffset = currentDate.getTimezoneOffset();
+    let minutesDifference = (fromTime - toTime) + currentTimeZoneOffset;
+    console.log(minutesDifference)
+    if (minutesDifference > 0) {
+      setTimeDiff(minutesToHoursConverter(minutesDifference));
+
+    } else {
+
+      setTimeDiff('-'+ minutesToHoursConverter(minutesDifference));
+    }
+
+  });
+
+  function minutesToHoursConverter(minutesDifference) {
+    minutesDifference = Math.abs(minutesDifference);
+    let hour = Math.floor(minutesDifference / 60);
+    let minute = minutesDifference % 60;
+    let time = hour + ':' + minute;
+    return time;
   }
+  return (
+    <div className='container'>
+      <div className="row">
+        <h1>Time zone finder</h1>
+      </div>
+      <div className='row'>
+        <div className='six columns'>
+          <h4>Your time</h4>
+          <TimePicker
+            onTimeChange={(options) => {
+            setFromHour(parseInt(options.hour), 10);
+            setFromMinute(parseInt(options.minute), 10);
+          }}
+            time={fromHour + ':' + fromMinute}/>
+        </div>
+        <div className='six columns'>
+          <h4>Their time</h4>
+          <TimePicker
+            onTimeChange=
+            { (options) => { setToHour(parseInt(options.hour,10)); setToMinute(parseInt(options.minute,10)); } }
+            time={toHour + ':' + toMinute}/>
+        </div>
+      </div>
+      <div className='row'>
+        {timeDiff}
+      </div>
+    </div>
+  )
 }
-
 export default App;
