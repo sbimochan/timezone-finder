7fb4190950aa49c76515c4a97218f7cbbddb62a9
sbimochan <bmochan@gmail.com>
2019-02-23 21:17:38 +0545

    Round up with 15minutes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './normalize.css';
 import './skeleton.css';
-import React, {useState, useEffect} from 'react';
-import TimePicker from 'react-times';
-
-// use material theme
 import 'react-times/css/material/default.css';
-// or you can use classic theme
 import 'react-times/css/classic/default.css';
+import React, {useState, useEffect} from 'react';
+
+import TimePicker from 'react-times';
+import {timezoneOffset} from './timezoneOffset';
 
 function App() {
   const [hour,
@@ -22,38 +21,34 @@ function App() {
     let toTime = hour * 60 + minute;
     let currentTimeZoneOffset = currentDate.getTimezoneOffset();
     let minutesDifference = (fromTime - toTime) + currentTimeZoneOffset;
+    console.log(new Date())
     if (minutesDifference > 0) {
-      
-      setTimeDiff('-'+ minutesToHoursConverter(minutesDifference));
+      setTimeDiff('-' + timeTransformer(minutesDifference));
     } else {
-      
-      setTimeDiff(minutesToHoursConverter(minutesDifference));
+      setTimeDiff(timeTransformer(minutesDifference));
     }
 
   });
 
-  function minutesToHoursConverter(minutesDifference) {
+  function timeTransformer(minutesDifference) {
     minutesDifference = Math.abs(minutesDifference);
     let hour = Math.floor(minutesDifference / 60);
     let minute = minutesDifference % 60;
-    let time = hour + ':' + minute;
-    return time;
+    let roundUp = Math.round(minute / 15) * 15
+    if (roundUp) {
+      let time = hour + '.' + roundUp;
+      return time
+    } else {
+      return hour
+    }
   }
+  
   return (
     <div className='container'>
       <div className="row">
         <h1>Time zone finder</h1>
       </div>
       <div className='row'>
-        {/* <div className='six columns'>
-          <h4>Your time</h4>
-          <TimePicker
-            onTimeChange={(options) => {
-            setFromHour(parseInt(options.hour), 10);
-            setFromMinute(parseInt(options.minute), 10);
-          }}
-            time={fromHour + ':' + fromMinute}/>
-        </div> */}
         <div className='six columns'>
           <h4>Their time</h4>
           <TimePicker
