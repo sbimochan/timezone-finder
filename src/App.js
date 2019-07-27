057964c8377e44e1907ac12c9b97443d5340eb18
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-07-27 20:24:59 +0545

    fixed compiling , warning issue on use effect while setting time diff, kept the loop fixer for time zone

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     } else {
       setTimeDiff(timeTransformer(minutesDifference));
     }
-  });
+  }, [meridiem, hour, minute]);
 
   useEffect(() => {
 		let timezone = timezoneOffset.filter(el => el.offset === timeDiff);
