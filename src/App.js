166af936d756b03a1ee28043f81066b44bc76d35
skj <sujan@innovatetech.co>
2019-09-10 10:31:46 +0545

    Add second argument in useEffect to handle state update error

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import './normalize.css';
-import './skeleton.css';
-import 'react-times/css/material/default.css';
-import React, {useState, useEffect} from 'react';
+import "./normalize.css";
+import "./skeleton.css";
+import "react-times/css/material/default.css";
+import React, { useState, useEffect } from "react";
 
-import TimePicker from 'react-times';
-import {timezoneOffset} from './timezoneOffset';
+import TimePicker from "react-times";
+import { timezoneOffset } from "./timezoneOffset";
 
 function App() {
-  const [hour,
-    setHour] = useState(10);
-  const [minute,
-    setMinute] = useState(42);
-  const [meridiem,
-    setMeridiem] = useState('PM');
-  const [timeDiff,
-    setTimeDiff] = useState(0);
-  const [offset,
-    setOffset] = useState([]);
+  const [hour, setHour] = useState(10);
+  const [minute, setMinute] = useState(42);
+  const [meridiem, setMeridiem] = useState("PM");
+  const [timeDiff, setTimeDiff] = useState(0);
+  const [offset, setOffset] = useState([]);
+  const [options, setOptions] = useState({});
 
+  // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     let currentDate = new Date();
     let fromTime = currentDate.getHours() * 60 + currentDate.getMinutes();
     let toTime = hour * 60 + minute;
     let currentTimeZoneOffset = currentDate.getTimezoneOffset();
-    let minutesDifference = (fromTime - toTime) + currentTimeZoneOffset;
+    let minutesDifference = fromTime - toTime + currentTimeZoneOffset;
     if (minutesDifference > 0) {
-      setTimeDiff(parseInt('-' + timeTransformer(minutesDifference)), 10);
+      setTimeDiff(parseInt("-" + timeTransformer(minutesDifference), 10), [
+        minutesDifference
+      ]);
     } else {
       setTimeDiff(timeTransformer(minutesDifference));
     }
-  });
+  }, [options]);
+
   useEffect(() => {
     let timezone = timezoneOffset.filter(el => el.offset === timeDiff);
     if (timezone) {
@@ -37,7 +37,7 @@ function App() {
     } else {
       setOffset([]);
     }
-  })
+  }, [options]);
 
   function timeTransformer(minutesDifference) {
     minutesDifference = Math.abs(minutesDifference);
@@ -45,7 +45,7 @@ function App() {
     let minute = minutesDifference % 60;
     let roundUp = Math.round(minute / 15) * 15;
     if (roundUp) {
-      let time = hour + '.' + roundUp;
+      let time = hour + "." + roundUp;
       return parseInt(time, 10);
     } else {
       return hour;
@@ -53,31 +53,38 @@ function App() {
   }
 
   return (
-    <div className='container'>
+    <div className="container">
       <div className="row mb-30">
         <h3>Time zone finder</h3>
       </div>
-      <div className='row'>
-        <div className='six columns'>
+      <div className="row">
+        <div className="six columns">
           <TimePicker
             focused
-            onTimeChange=
-            { (options) => { setHour(parseInt(options.hour, 10)); setMinute(parseInt(options.minute, 10)); setMeridiem(options.meridiem);} }
-            time={hour + ':' + minute}
+            onTimeChange={options => {
+              setOptions(options);
+              setHour(parseInt(options.hour, 10));
+              setMinute(parseInt(options.minute, 10));
+              setMeridiem(options.meridiem);
+            }}
+            time={hour + ":" + minute}
             meridiem={meridiem}
             theme="material"
             colorPalette="dark"
             timeMode="12"
             showTimezone={true}
-            closeOnOutsideClick={false}/>
+            closeOnOutsideClick={false}
+          />
         </div>
         <div className="six columns">
           <ul>
-            {offset.map(el => <li>{el.utc + ' ' + el.text}</li>)}
+            {offset.map(el => (
+              <li key={el.text}>{el.utc + " " + el.text}</li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 export default App;
